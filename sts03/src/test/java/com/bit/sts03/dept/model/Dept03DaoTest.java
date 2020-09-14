a3de993a713cb458faa8e01c5ab0f7878e4dd8f7
bit01class <bit01class@gmail.com>
2020-09-14 13:11:57 +0900

    spring3.x - TDD기반 개발 dao test

--- sts03/src/test/java/com/bit/sts03/dept/model/Dept03DaoTest.java
+++ sts03/src/test/java/com/bit/sts03/dept/model/Dept03DaoTest.java
@@ -1,8 +1,9 @@
 package com.bit.sts03.dept.model;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.sql.Connection;
@@ -14,12 +15,18 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.bit.sts03.dept.model.entity.Dept03Vo;
-
+//@FixMethodOrder
+//@FixMethodOrder(MethodSorters.DEFAULT)		테스트로 출력을 할 수 있지만 순서가 보장되지 않습니다.
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)	메소드명 기준으로하여 오름차순으로 실행됩니다.
+//@FixMethodOrder(MethodSorters.JVM)			테스트방법을 JVM이 리턴한 순서대로 실행됩니다. 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Dept03DaoTest {
 	static Dept03Vo target=new Dept03Vo(1,"test","test");
 	Dept03Dao dept03Dao;
@@ -37,13 +44,11 @@ public class Dept03DaoTest {
 		ac=new ClassPathXmlApplicationContext("/applicationContext.xml");
 		DataSource dataSource=(DataSource) ac.getBean("dataSource");
 		Connection conn=dataSource.getConnection();
-		System.out.println(sql);
 		/* create table */
 		conn.prepareStatement(sql).execute();
-		/* insert dummy data */
-		
-		conn.prepareStatement("insert into dept03 (dname,loc) values ('"
-		+target.getDname()+"','"+target.getLoc()+"')").executeUpdate();
+//		/* insert dummy data */
+//		conn.prepareStatement("insert into dept03 (dname,loc) values ('"
+//		+target.getDname()+"','"+target.getLoc()+"')").executeUpdate();
 	}
 
 	@AfterClass
@@ -66,14 +71,35 @@ public class Dept03DaoTest {
 
 	@Test
 	public void testSelectAll() {
-		System.out.println("testSelectAll...");
 		assertSame(1, dept03Dao.selectAll().size());
 	}
 	
 	@Test
+	public void testInsertOne() {
+		try {
+			dept03Dao.insertOne(target);
+			assertTrue(true);
+		} catch (SQLException e) {
+			assertFalse(true);
+		}
+	}
+	
+	@Test
 	public void testSelectOne() {
-		System.out.println("testSelectOne...");
 		assertEquals(target, dept03Dao.selectOne(target.getDeptno()));
+	}
+	
+	@Test
+	public void testUpdate() throws SQLException {
+//		target=new Dept03Vo(target.getDeptno(),"test1","test2");
+		target.setDname("test1");
+		target.setLoc("test2");
+		assertSame(1, dept03Dao.updateOne(target));
+	}
+	
+	@Test
+	public void testZDeleteOne() throws SQLException {
+		assertSame(1, dept03Dao.zDeleteOne(target.getDeptno()));
 	}
 	
 }
