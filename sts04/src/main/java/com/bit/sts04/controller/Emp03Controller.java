3264e0c8e605e219423e3ccb3a78dce7d949401f
bit01class <bit01class@gmail.com>
2020-09-15 17:55:18 +0900

    Spring 3.x - ibatis&mybatis

--- sts04/src/main/java/com/bit/sts04/controller/Emp03Controller.java
+++ sts04/src/main/java/com/bit/sts04/controller/Emp03Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,6 +36,12 @@ public class Emp03Controller {
 			return "emp/add";
 		}
 		return "redirect:./";
+	}
+	@RequestMapping("/{sabun}")
+	public String detail(@PathVariable int sabun,Model model) throws SQLException {
+		model.addAttribute("bean", empDao.selectOne(sabun));
+		model.addAttribute("title", "Detail");
+		return "emp/add";
 	}
 }
 
