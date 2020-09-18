ce53a45ec548af422c56cc2843610bded8b337ce
bit01class <bit01class@gmail.com>
2020-09-18 11:32:20 +0900

    Restful service

---
+++ sts07/src/main/java/com/bit/sts07/service/DeptServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bit.sts07.service;
+
+import java.sql.SQLException;
+
+import org.springframework.ui.Model;
+
+public class DeptServiceImpl implements DeptService {
+
+	@Override
+	public void listService(Model model) throws SQLException{
+		
+	}
+}
