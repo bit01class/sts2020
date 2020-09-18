78f59ade1a7ce177812ad4503dd6bf0c3632096a
bit01class <bit01class@gmail.com>
2020-09-18 12:41:48 +0900

    restful mybatis GettingStarted

--- sts07/src/main/java/com/bit/sts07/service/DeptServiceImpl.java
+++ sts07/src/main/java/com/bit/sts07/service/DeptServiceImpl.java
@@ -2,12 +2,24 @@ package com.bit.sts07.service;
 
 import java.sql.SQLException;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import com.bit.sts07.model.DeptDao;
+
+@Service
 public class DeptServiceImpl implements DeptService {
+	@Inject
+	SqlSession sqlSession;
 
 	@Override
 	public void listService(Model model) throws SQLException{
-		
+		DeptDao dao = sqlSession.getMapper(DeptDao.class);
+		System.out.println(dao);
+		model.addAttribute("list", dao.selectAll());
+//		model.addAttribute("list", sqlSession.selectList("selectAll"));
 	}
 }
