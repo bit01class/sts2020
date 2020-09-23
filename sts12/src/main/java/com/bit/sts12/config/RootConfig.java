7ce37399b8867a337cc64b34d1e39c22bbdab07c
bit01class <bit01class@gmail.com>
2020-09-23 16:01:22 +0900

    dept api service(list)

--- sts12/src/main/java/com/bit/sts12/config/RootConfig.java
+++ sts12/src/main/java/com/bit/sts12/config/RootConfig.java
@@ -2,12 +2,18 @@ package com.bit.sts12.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mariadb.jdbc.Driver;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
+@MapperScan("com.bit.sts12.dept.model")
 public class RootConfig {
 
 	/*
@@ -28,6 +34,18 @@ public class RootConfig {
 		dataSource.setUsername("scott");
 		dataSource.setPassword("tiger");
 		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource());
+		return bean.getObject();
+	}
+	
+	@Bean
+	public SqlSession sqlSession() throws Exception {
+		return new SqlSessionTemplate(sqlSessionFactory());
 	}
 }
 
